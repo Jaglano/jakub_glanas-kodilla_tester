e82009440a7481611fcb7562ef26f988e68804bc
Jakub Glanas <jakub.glanas@gmail.com>
2022-08-11 20:20:52 +0200

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main (String [] args) {
+        int wiek = 26;
+        char litera = 'G';
+        System.out.println("Hello World");
+        System.out.println("Kuba");
+        System.out.println(wiek);
+        System.out.print(litera);
+
+    }
+}
+

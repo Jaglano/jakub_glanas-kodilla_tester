4b97d2ebfdf2dd738a0925d01dbb5cb917046ba1
Jakub Glanas <jakub.glanas@gmail.com>
2022-08-12 09:43:17 +0200

    Klasa Main, MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Kuba";
+        int wiek = 26;
+        double height = 1.77;
+        char sex = 'M';
+        String miasto = "Kraków";
+        boolean isHappy = true;
+
+            System.out.print(myName);
+            System.out.print(wiek);
+            System.out.print(height);
+            System.out.print(sex);
+            System.out.print(miasto);
+            System.out.print(isHappy);
+    }
+}

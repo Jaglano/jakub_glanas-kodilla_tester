a70a5178bc686f94b7d1fbc6d153e27496c4e74c
Jakub Glanas <jakub.glanas@gmail.com>
2022-10-13 17:45:34 +0200

    6.2 - Zadanie: uczniowie i nauczyciele

--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class Application {
-    public static void main(String[] args) {
+public class Application {void main(String[] args) {
         List<Student> students = new ArrayList<>();
         students.add(new Student("Jerzy", new Teacher("Urban")));
         students.add(new Student("Aleksander", new Teacher("Nowak")));
@@ -17,5 +16,12 @@ public class Application {
             System.out.println("uczeń: " + student.getName() + ", nauczyciel: " + teacher.getName());
         }
 
+    }
+
+    public static String getTeacherName(Student student) {
+        Optional<Teacher> optionalTeacher = Optional.ofNullable(student.getTeacher());
+        String undefined =
+                optionalTeacher.orElse(new Teacher("<undefined>")).getName();
+        return undefined;
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTest.java
+++ kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTest.java
@@ -8,26 +8,19 @@ import java.util.List;
 import java.util.Optional;
 
 
+
 public class ApplicationTest {
-
-    public static List<Student> getStudentList() {
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Jerzy", new Teacher("Urban")));
-        students.add(new Student("Aleksander", new Teacher("Nowak")));
-        students.add(new Student("Marek", null));
-        students.add(new Student("Bob", new Teacher("Dylan")));
-
-        for (Student student:students) {
-            Teacher teacher = Optional.ofNullable(student.teacher).orElse(new Teacher("<undefinied"));
-            System.out.println("uczeń: " + student.getName() + ", nauczyciel: " + teacher.getName());
-        }
-        return students;
-    }
-
     @Test
-    public void nullTeacher(){
-        getStudentList();
-        assertEquals(null, getStudentList().containsAll());
+    public void shouldReturnTeacherName() {
+        Teacher teacher = new Teacher("Bob");
+        Student arek = new Student("Arek", teacher);
+        Application.getTeacherName(arek);
+        assertEquals("Bob", Application.getTeacherName(arek));
     }
-
+    @Test
+    public void shouldReturnNullTeacherName() {
+        Student robert = new Student("Robert", null);
+        Application.getTeacherName(robert);
+        assertEquals("<undefined>", Application.getTeacherName(robert));
+    }
 }

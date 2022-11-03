737000dc1c48e60ca4e7af324d6f1263938299c1
Jaglano <111054302+Jaglano@users.noreply.github.com>
2022-11-03 18:08:52 +0100

    9.3. Spring – ćwiczenia

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
@@ -3,29 +3,24 @@ package com.kodilla.spring.basic.spring_dependency_injection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+
 @Component
 public class SimpleApplication {
 
-    private SkypeMessageService messageService;
 
-    public String processMessage(String message, String receiver){
+    @Resource(name = "skypeMessageService")
+    private MessageService messageService;
+
+    public String processMessage(String message, String receiver) {
         if (checkReceiver(receiver)) {
             return this.messageService.send(message, receiver);
         }
         return null;
     }
 
-    private boolean checkReceiver(String receiver){
+    private boolean checkReceiver(String receiver) {
         return receiver != null && !receiver.isEmpty();
-    }
-
-    public SkypeMessageService getMessageService() {
-        return messageService;
-    }
-
-    @Autowired
-    public void setMessageService(SkypeMessageService messageService) {
-        this.messageService = messageService;
     }
 }
 

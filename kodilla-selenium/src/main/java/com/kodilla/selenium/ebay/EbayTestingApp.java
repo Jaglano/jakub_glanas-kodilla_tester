86a93961772ad87afb41a0e135fc4175390155ce
Jaglano <111054302+Jaglano@users.noreply.github.com>
2022-11-29 20:47:41 +0100

    13.3. Selenium Webdriver — pierwsze testy

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EbayTestingApp.java
@@ -0,0 +1,24 @@
+package com.kodilla.selenium.ebay;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class EbayTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com/");
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("gh-td-s")));
+
+
+        WebElement inputfield = driver.findElement(By.name("_nkw"));
+        inputfield.sendKeys("Laptop");
+        inputfield.submit();
+    }
+}

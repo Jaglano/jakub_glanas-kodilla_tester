698db82533496c67a61ffd8aeec57d9df7961f7a
Jaglano <111054302+Jaglano@users.noreply.github.com>
2022-11-30 11:44:15 +0100

    13.5 Zadanie: Ćwiczymy selektory CSS

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingAppCssSelector.java
@@ -0,0 +1,36 @@
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class AllegroTestingAppCssSelector {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://allegro.pl/");
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+
+
+        driver.findElement(By.cssSelector("button[data-role*=accept-consent]")).click();
+        WebElement category = driver.findElement(By.cssSelector("select[data-role*=filters-dropdown-toggle]"));
+        Select categorySelect = new Select(category);
+        categorySelect.selectByVisibleText("Elektronika");
+        WebElement inpuField = driver.findElement(By.cssSelector("input[data-role*=search-input]"));
+        inpuField.sendKeys("Mavic mini");
+        inpuField.submit();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("section > article")));
+        List<WebElement> element = driver.findElements(By.cssSelector("section > article"));
+        for (WebElement elementList : element) {
+            System.out.println(elementList.getText());
+            System.out.println("/----Next article---/");
+
+        }
+    }
+}
